b20807b7c7d02f129091feaa629c95511a345bfd
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-07-20 18:15:32 +0530

    Made basic connects of mongoose and node js

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const express = require('express');
-
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+const connectDB = async () => {
+  mongoose.connect(
+    "mongodb+srv://pathaktaru2002:jHDZVsfhjjTYuoPC@cluster0.ti2tfsl.mongodb.net/e-comm"
+  );
+  const productSchema = new mongoose.Schema({});
+  const product = mongoose.model("product", productSchema);
+  const data = await product.find();
+  console.log(data);
+};
 
-app.get('/',(req,resp)=>{
-    resp.send("app is working");
-})
+connectDB();
 
 app.listen(5000);
