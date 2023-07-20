1ea0f3fe7a48e462e9422e6dcc1ac48d78955959
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-07-20 23:02:36 +0530

    Added login api

--- index.js
+++ index.js
@@ -9,7 +9,22 @@ app.use(cors());
 app.post("/register", async (req, resp) => {
   let uuser = await new user(req.body);
   let result = await uuser.save();
+  result= result.toObject();
+  delete result.password;
   resp.send(result);
 });
- 
+
+app.post("/login", async (req, resp) => {
+  if (req.body.password && req.body.email) {
+    let uuser = await user.findOne(req.body).select("-password");
+    if (uuser) {
+      resp.send(uuser);
+    } else {
+      resp.send({ result: "No user found" });
+    }
+  } else {
+    resp.send({ result: "No user found" });
+  }
+});
+
 app.listen(5000);
