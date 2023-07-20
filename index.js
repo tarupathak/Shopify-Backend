4360b94ff32dfb27978f4de331a740e929b9e603
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-07-20 18:43:24 +0530

    Resolved cors issue

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 require("./db/config");
 const user = require("./db/user");
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.post("/register", async (req, resp) => {
   let uuser = await new user(req.body);
   let result = await uuser.save();
   resp.send(result);
 });
-
+ 
 app.listen(5000);
